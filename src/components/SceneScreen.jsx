cf5a730a43adabaf8581e0fb813c5685f40fb694
hiranaru <n-hirano@sbh-sendai.com>
2025-08-05 11:09:59 +0900

    Create SceneScreen.jsx

---
+++ src/components/SceneScreen.jsx
@@ -0,0 +1,16 @@
+// src/components/SceneScreen.jsx
+import dummyScene from '../assets/dummy-scene.jpg'
+import '../styles/SceneScreen.css'
+
+export default function SceneScreen({ onCorrect }) {
+  return (
+    <div className="scene-screen">
+      <img src={dummyScene} alt="シーン画像" className="scene-image" />
+      <button
+        className="hit-area"
+        onClick={onCorrect}
+        aria-label="正解エリア"
+      ></button>
+    </div>
+  )
+}
