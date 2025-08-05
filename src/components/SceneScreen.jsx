bd249b8ca5cfe4d46aec5e66a77ba5f2b6068f9c
hiranaru <n-hirano@sbh-sendai.com>
2025-08-05 11:19:27 +0900

    Update SceneScreen.jsx

--- src/components/SceneScreen.jsx
+++ src/components/SceneScreen.jsx
@@ -1,11 +1,10 @@
 // src/components/SceneScreen.jsx
-import dummyScene from '../dummy-scene.jpg'
 import '../styles/SceneScreen.css'
 
 export default function SceneScreen({ onCorrect }) {
   return (
     <div className="scene-screen">
-      <img src={dummyScene} alt="シーン画像" className="scene-image" />
+      <img src="/dummy-scene.jpg" alt="シーン画像" className="scene-image" />
       <button
         className="hit-area"
         onClick={onCorrect}
